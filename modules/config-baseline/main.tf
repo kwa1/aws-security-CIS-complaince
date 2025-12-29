b6a0c0ffce062318ff5d0379cc9593329b2d2c0c
Dennis Amankwa <amankwa32@hotmail.com>
2025-12-29 11:07:50 +0000

    Add AWS Config IAM role and configuration resources

---
+++ modules/config-baseline/main.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role" "config" {
+  name = "aws-config-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "config.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.config.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
+
+resource "aws_config_configuration_recorder" "this" {
+  name     = "config-recorder"
+  role_arn = aws_iam_role.config.arn
+
+  recording_group {
+    all_supported                 = true
+    include_global_resource_types = true
+  }
+}
+
+resource "aws_config_delivery_channel" "this" {
+  name           = "config-delivery"
+  s3_bucket_name = var.config_bucket
+}
+
+resource "aws_config_configuration_recorder_status" "enabled" {
+  name       = aws_config_configuration_recorder.this.name
+  is_enabled = true
+}
