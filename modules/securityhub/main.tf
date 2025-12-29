cd6cd40b54c5eeaf7548238e657026a59647c440
Dennis Amankwa <amankwa32@hotmail.com>
2025-12-29 11:26:20 +0000

    Add AWS Security Hub resources for account and CIS standards

---
+++ modules/securityhub/main.tf
@@ -0,0 +1,5 @@
+resource "aws_securityhub_account" "this" {}
+
+resource "aws_securityhub_standards_subscription" "cis" {
+  standards_arn = "arn:aws:securityhub:::standards/cis-aws-foundations-benchmark/v/1.4.0"
+}
