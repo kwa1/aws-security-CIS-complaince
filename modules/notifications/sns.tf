2e9814ef1151f0c6d7df36bbc834d23816ee957e
Dennis Amankwa <amankwa32@hotmail.com>
2025-12-29 13:22:49 +0000

    Add SNS topic and optional email subscription

---
+++ modules/notifications/sns.tf
@@ -0,0 +1,11 @@
+resource "aws_sns_topic" "config_alerts" {
+  name = "aws-config-security-alerts"
+}
+
+# Optional email subscription (for audits)
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.config_alerts.arn
+  protocol  = "email"
+  endpoint  = var.security_email
+}
