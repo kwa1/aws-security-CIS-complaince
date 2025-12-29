6fb8b8a77663fede3575b48823222af5ea9255f3
Dennis Amankwa <amankwa32@hotmail.com>
2025-12-29 15:33:00 +0000

    Add SNS topic subscription for Slack notifications

--- modules/notifications/sns.tf
+++ modules/notifications/sns.tf
@@ -12,6 +12,11 @@ resource "aws_sns_topic_subscription" "securityhub_email" {
   protocol  = "email"
   endpoint  = var.securityhub_email
 }
+resource "aws_sns_topic_subscription" "slack" {
+  topic_arn = aws_sns_topic.config_alerts.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.slack.arn
+}
 
 # ----------------------------
 # Variables
